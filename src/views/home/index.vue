f76dc70e536f0b0cdf26f72035f7c7d310107dc2
PJ <pj@MacBook-Pro.local>
2021-01-26 18:21:32 +0800

    init

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,61 @@
 <template>
-$END$
+  <div class="home">
+    <router-link class="item" to="/util">工具库</router-link>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'index',
-};
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+});
 </script>
+<style lang="scss" scoped>
+.home{
+  display:flex;
+  padding-top: 100px;
+  justify-content: center;
+}
+.item{
+  position: relative;
+  border: 1px solid #03A9F3;
+  width:150px;
+  height:50px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  font-weight:bold;
 
-<style scoped>
+  &::before,
+  &::after {
+    content: " ";
+    position: absolute;
+    width: 20px;
+    height: 20px;
+  }
 
+  &::before {
+    top: -5px;
+    left: -5px;
+    border-top: 1px solid #03A9F3;
+    border-left: 1px solid #03A9F3;
+    transition:all .4s;
+  }
+
+  &::after {
+    right: -5px;
+    bottom: -5px;
+    border-bottom: 1px solid #03A9F3;
+    border-right: 1px solid #03A9F3;
+    transition:all .4s;
+
+  }
+
+  &:hover::before,
+  &:hover::after {
+    width: calc(100% + 9px);
+    height: calc(100% + 9px);
+  }
+}
 </style>
--- src/views/util/index.vue
+++ src/views/util/index.vue
@@ -1,13 +1,46 @@
 <template>
-$END$
+<div class="handel">
+  <div class="button-container">
+    <button class="button">
+      上传文件
+    </button>
+  </div>
+
+</div>
 </template>
 
-<script>
-export default {
-  name: 'index',
-};
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Util',
+});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.handel{
+  padding:110px;
+}
+.button-container{
+  width:120px;
+  height:40px;
+  border-radius:12px;
+  //border: 1px solid #00ccff;
+  .button{
+    width:120px;
+    height:40px;
+    background-color: #00ccff;
+    border-radius:12px;
+    border: 0;
+    color: #ffffff;
+    font-weight: bold;
+    font-size:15px;
+    outline: none;
+  }
+
+  &:hover{
+    border: 1px solid #00ccff;
+  }
+}
 
 </style>
