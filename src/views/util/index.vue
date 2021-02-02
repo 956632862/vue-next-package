9efa2a31e432b1eef90b4f431711775766207d24
PJ <pj@MacBook-Pro.local>
2021-02-02 10:57:23 +0800

    init

--- src/views/util/index.vue
+++ src/views/util/index.vue
@@ -1,46 +1,22 @@
 <template>
-<div class="handel">
-  <div class="button-container">
-    <button class="button">
-      上传文件
-    </button>
-  </div>
-
+<div class="handle" >
+    <upload zone/>
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import upload from './components/upload/index.vue';
 
 export default defineComponent({
   name: 'Util',
+  components: { upload },
 });
 </script>
 
 <style lang="scss" scoped>
-.handel{
+.handle{
   padding:110px;
-}
-.button-container{
-  width:120px;
-  height:40px;
-  border-radius:12px;
-  //border: 1px solid #00ccff;
-  .button{
-    width:120px;
-    height:40px;
-    background-color: #00ccff;
-    border-radius:12px;
-    border: 0;
-    color: #ffffff;
-    font-weight: bold;
-    font-size:15px;
-    outline: none;
-  }
-
-  &:hover{
-    border: 1px solid #00ccff;
-  }
 }
 
 </style>
